54c4d6b31d74b6e66ac7ee2e1273a79365c1317d
rodan.rodanih2@bk.ru <rodan.rodanih2@bk.ru>
2020-04-08 19:48:34 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
